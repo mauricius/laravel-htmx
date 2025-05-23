d9cb3ec17fafce1d88b197e18b6592d11e4d552d
Muhammed Sari <mabdullahsari@hotmail.com>
2025-05-23 09:40:04 +0200

    Add .stubs.php for IDE support

---
+++ .stubs.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Illuminate\View
+{
+    class View
+    {
+        public function renderFragment(string $view, string $fragment, array $data = []): string {}
+    }
+}
